a686247b32a4ba4263af41c5e9973e4eebc334f7
Do Luong Phuong Sang <22521237@gm.uit.edu.vn>
2024-12-12 19:44:57 +0700

    huhu xong di ma

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,92 +1,3 @@
-// Function to store user information in local storage during signup
-function signup() {
-  var usernameInput = document.getElementById("username-signup").value;
-  var passwordInput = document.getElementById("password-signup").value;
-  var emailInput = document.getElementById("email-signup").value;
-
-  // Retrieve the existing users from local storage or create an empty array
-  var existingUsers = JSON.parse(localStorage.getItem("users")) || [];
-
-  // Check if the username is already taken
-  if (existingUsers.some((user) => user.username === usernameInput)) {
-    alert("Tên người dùng đã tồn tại. Vui lòng chọn tên khác.");
-    return;
-  }
-
-  // Create an object to store user information
-  var user = {
-    username: usernameInput,
-    password: passwordInput,
-    email: emailInput,
-  };
-
-  // Add the new user to the array
-  existingUsers.push(user);
-
-  // Convert the array of users to a JSON string and store it in local storage
-  localStorage.setItem("users", JSON.stringify(existingUsers));
-
-  document.getElementById("username-signup").value = "";
-  document.getElementById("password-signup").value = "";
-  document.getElementById("email-signup").value = "";
-
-  alert("Chúc mừng bạn đã đăng ký thành công");
-}
-function signin() {
-  var usernameInput = document.getElementById("username").value;
-  var passwordInput = document.getElementById("password").value;
-  var usernameShow = document.getElementById("username_show");
-  var userIcon = document.querySelector("#user i");
-
-  // Retrieve user information from local storage
-  var existingUsers = JSON.parse(localStorage.getItem("users")) || [];
-
-  // Check if the entered username and password match any user in the array
-  var foundUser = existingUsers.find(
-    (user) => user.username === usernameInput && user.password === passwordInput
-  );
-
-  // Check if user information exists in local storage
-  if (foundUser) {
-    // Store login information in local storage
-    localStorage.setItem("login", JSON.stringify(foundUser));
-
-    // Thay đổi nội dung và hiển thị tên người dùng
-    if (usernameInput.length > 7) {
-      usernameShow.innerText = usernameInput.slice(0, 6) + "...";
-    } else {
-      usernameShow.innerText = usernameInput;
-    }
-
-    // Ẩn icon user
-    userIcon.style.display = "none";
-    usernameShow.style.display = "block";
-
-    // Đóng popup sau khi đăng nhập
-    var login_popup = document.getElementById("loginPopup");
-    login_popup.style.display = "none";
-  } else {
-    // Handle the case where no user information is found in local storage
-    alert("Không tìm thấy thông tin người dùng. Vui lòng đăng ký trước.");
-  }
-
-  document.getElementById("username").value = "";
-  document.getElementById("password").value = "";
-}
-// Function to handle logout
-function logout() {
-  var usernameShow = document.getElementById("username_show");
-  var userIcon = document.querySelector("#user i");
-  var userPopup = document.getElementById("userPopup");
-
-  // Remove login information from local storage
-  localStorage.removeItem("login");
-
-  // Ẩn icon user
-  userIcon.style.display = "contents";
-  usernameShow.style.display = "none";
-  userPopup.style.display = "none";
-}
 // Function to set up the user display on page load
 function setupUserDisplay() {
   var usernameShow = document.getElementById("username_show");
@@ -110,30 +21,6 @@ function setupUserDisplay() {
     // Ẩn icon user
     userIcon.style.display = "none";
     usernameShow.style.display = "block";
-  }
-}
-function sendEmail() {
-  alert("Chúng tôi đã gửi mã đổi password đến địa chỉ mail của bạn!");
-}
-function flip() {
-  document.querySelector("#flipper").classList.toggle("flip");
-}
-
-$(document).ready(function () {
-  $("#username_show").click(function () {
-    $("#userPopup").slideToggle();
-  });
-});
-
-function closeUserPopUp(event) {
-  var userContainer = document.getElementById("user-popup-container");
-  var userPopup = document.getElementById("userPopup");
-  if (
-    !userContainer.contains(event.target) &&
-    !event.target.classList.contains("ghost")
-  ) {
-    // Đóng popup
-    userPopup.style.display = "none";
   }
 }
 
@@ -397,13 +284,13 @@ function Thanhtoan(event) {
 }
 $(document).ready(function () {
   $(".search_icon").click(function (event) {
-    if ($(".hidden_input").css("display") === "none") {
+    if ($(".hidden").css("display") === "none") {
       event.preventDefault();
-      $(".hidden_input").show();
+      $(".hidden").show();
       $(".header_right_search").css("border-bottom", "1px solid black");
       $(".header_right_search i").css("top", "0%");
     } else {
-      window.location.href = "../template/ghe.html";
+      window.location.href = "./sanpham_tarot.html";
     }
   });
 });
